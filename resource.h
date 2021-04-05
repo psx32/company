b23ca5131b00f98d03ac807b0306baa77ec96305
psx32 <marco.aasheim@gmail.com>
2021-04-05 18:24:45 +0000

    test

---
+++ resource.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+/* The cost and percentages of different
+ * resources in a company: in $ and % */
+
+// Resources in costs
+float equipment = 		98000.000;
+float incorporationFees = 	268.000;
+float officeSpace = 		654.000;	// Per month
+float website = 		41.000;		// Per month
+float utilites = 		2.00;		// per square foot
+float profConsultant = 		3500.000;	// Per year
+float insurance = 		1200.000;	// Per year
+
+// Resources in percentages
+float inventory = 	0.21; 	// 21%
+float marketing = 	0.045;	// 4.5%
+float supplies = 	0.10;	// 10%
+float payroll = 	0.385;	// 38.5%
+float corpTaxes =	0.21;	// 21% 		
+
+
+#endif
